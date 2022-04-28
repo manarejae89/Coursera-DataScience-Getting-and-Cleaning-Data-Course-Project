b14e0bbb14449691cd2346c730a2af6c96fa727f
manarejae89 <100319175+manarejae89@users.noreply.github.com>
2022-04-28 10:06:48 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,77 @@
+==================================================================
+Analysis of Human Activity Recognition Using Smartphones Data Set
+==================================================================
+Manuel J. Arenas
+manarejae@gmail.com
+https://sites.google.com/view/manueljarenas/
+==================================================================
+
+Data Set description:
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+Data Set download link:
+http://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI%20HAR%20Dataset.zip
+
+======================================
+---------------------
+run_analysis.R output:
+---------------------
+data: merge data from Train and Test experiments
+
+groupdata: "data" grouped by Subject Id and Training Type
+
+summgroupdata: summary of "groupdata" with the mean of each measurement
+
+activity_labels: levels and labels for Training Type
+
+features: labels for measurements
+
+---------------------
+run_analysis.R do as follows:
+---------------------
+# Load "dplyr" library
+
+###
+# Import the following data with the "read.table" command from the "UCI HAR Dataset" folder
+###
+# Import Activity Labels
+
+# Import Features [names]
+
+# Import Train Data [SubjectId, TestLabel, X-Measurements]
+# Import Test Data  [SubjectId, TestLabel, X-Measurements]
+
+###
+# 1. Merges the training and the test sets to create one data set.
+###
+# "Merge" Train and Test Data with "rbind" command
+# Remove unnecessary imported Data
+# "Merge" all data with "cbind" command
+# Remove unnecessary imported Data
+# Renaming of variables by column (usage of "features")
+
+###
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+###
+# Using "grep" command
+
+###
+# 3. Uses descriptive activity names to name the activities in the data set
+###
+# TestLabel -> Change numberic to string labels from "activity_labels"
+
+###
+# 4. Appropriately labels the data set with descriptive variable names
+###
+# Nicer variable names in "data" (remove "()" and "-") with "gsub" command
+
+###
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+###
+
+# Group data by SubjectId and TestLabel with "group_by" command
+# Summarize data with the mean of each feature/measurement column with "summarize" command
+# Cleaning column names (remove "_1") with "gsub" command
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,76 @@
+# Load "dplyr" library
+library(dplyr)
+
+# Import Activity Labels
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", quote="\"", comment.char="")
+
+# Import Features [names]
+features <- read.table("UCI HAR Dataset/features.txt", row.names=1, quote="\"", comment.char="")
+names(features)="V1"
+
+# Import Train Data
+# SubjectId
+SubjectId_train <- read.table("UCI HAR Dataset/train/subject_train.txt", quote="\"", comment.char="")
+# TestLabel
+TestLabel_train <- read.table("UCI HAR Dataset/train/y_train.txt", quote="\"", comment.char="")
+# X - Measurements
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt", quote="\"", comment.char="")
+
+# Import Test Data
+# SubjectId
+SubjectId_test <- read.table("UCI HAR Dataset/test/subject_test.txt", quote="\"", comment.char="")
+# TestLabel
+TestLabel_test <- read.table("UCI HAR Dataset/test/y_test.txt", quote="\"", comment.char="")
+# X - Measurements
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt", quote="\"", comment.char="")
+
+###
+# 1. Merges the training and the test sets to create one data set.
+###
+# "Merge" Train and Test Data
+SubjectId<-rbind(SubjectId_train,SubjectId_test)
+TestLabel<-rbind(TestLabel_train,TestLabel_test)
+X<-rbind(X_train,X_test)
+# Remove unnecessary imported Data
+rm(TestLabel_test,SubjectId_test,X_test,TestLabel_train,SubjectId_train,X_train)
+
+# "Merge" all data
+data<-cbind(SubjectId,TestLabel,X)
+# Remove unnecessary imported Data
+rm(SubjectId,TestLabel,X)
+# Naming of variables by column (usage of "features")
+names(data)=c("SubjectId","TestLabel",features$V1)
+
+###
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+###
+meanstdindex <- grep(pattern = 'mean|std', x = names(data))
+data<-data[,c(1,2,meanstdindex)]
+
+###
+# 3. Uses descriptive activity names to name the activities in the data set
+###
+# TestLabel -> Change numberic to string labels from "activity_labels"
+for (i in activity_labels$V1){
+  data$TestLabel <- replace(data$TestLabel, data$TestLabel==activity_labels$V1[i], activity_labels$V2[i])
+}
+
+###
+# 4. Appropriately labels the data set with descriptive variable names
+###
+# Nicer variable names in "data" (remove "()" and "-")
+names(data)[3:length(data)]<-gsub("[()]|[-]", "", names(data)[3:length(data)])
+
+###
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+###
+
+# Group data by SubjectId and TestLabel
+groupdata<-group_by(data, SubjectId, TestLabel)
+# Summarize data with the mean of each feature/measurement column
+summgroupdata<-summarize(groupdata,across(everything(), list(mean)))
+# Cleaning column names (remove "_1")
+names(summgroupdata)[3:length(summgroupdata)]<-gsub("[_1]", "", names(summgroupdata)[3:length(summgroupdata)])
+
+# Write result to .txt file
+#write.table(summgroupdata,file ="summgroupdata.txt",row.name=FALSE)
